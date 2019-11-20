d7e0ca5fcf1b50a413076f6f93095b7326375f7a
Sanya <sanya@sanyaiv.com>
2019-11-20 20:14:21 +0100

    Created base UE project without started content

---
+++ Source/Arbetsprov/ArbetsprovGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArbetsprovGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARBETSPROV_API AArbetsprovGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
